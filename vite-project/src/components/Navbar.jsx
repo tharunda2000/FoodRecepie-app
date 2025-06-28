f804eccd28f3059a76636e615488c576fb5cdac1
Tharunda De Silva <tharundadesilva@gmail.com>
2025-06-28 15:13:33 +0530

    home button and profile button added to navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Search from './Search'
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = ({foodData,setFoodData}) => {
+
+  const navigate = useNavigate();
+
   return (
     <div className=' fixed top-0 left-0 flex bg-green-400 h-30 w-full  mb-5 shadow-lg justify-between items-center'>
 
-        <i className="ri-restaurant-fill text-5xl ms-20 text-green-800  font-extrabold text-shadow-sm text-shadow-green-950 "></i>
-        <h1 className=' hidden sm:flex text-green-900 text-4xl font-extrabold text-shadow-md text-shadow-green-950' >Let's make it</h1>
+        <i className="ri-restaurant-fill text-5xl ms-20 text-green-800  font-extrabold text-shadow-sm text-shadow-green-950 cursor-pointer" onClick={()=>{navigate('/')} }></i>
+        <h1 className=' hidden sm:flex text-green-900 text-4xl font-extrabold text-shadow-md text-shadow-green-950 cursor-pointer' onClick={()=>{navigate('/')} }>Let's make it</h1>
         
         
           <Search  setFoodData={setFoodData}/>
--- vite-project/src/components/Search.jsx
+++ vite-project/src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect,useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const URL = "https://api.spoonacular.com/recipes/complexSearch"
 const apiKey = "1326c5cd15c145e187ae20ef2ca01df1"
@@ -7,29 +8,38 @@ const Search = ({setFoodData}) => {
 
     const [query,setQuery] = useState("");
     const [inputValue,setInputValue] = useState("");
+    const navigate = useNavigate();
 
     useEffect(()=>{
         const fetchFood = async () => {
+            
             const  res = await fetch(`${URL}?query=${query}&apiKey=${apiKey}`)
             const data = await res.json()
+             
             setFoodData(data.results)
             
 
         }
         fetchFood();
+        
     },[query])
 
     const handleKeyDown = (e) =>{
       if(e.key === 'Enter'){
-        setQuery(inputValue);
+        
+        setQuery(inputValue)
+        navigate('/search');
       }
+      
 
     }
 
   return (
     <div className='flex me-10 items-center gap-2'>
-      <input type="text" className='rounded-3xl bg-green-200 py-2 px-4 border-1 border-green-600  w-60' placeholder='Search'  onChange={(e)=>setInputValue(e.target.value)} onKeyDown={handleKeyDown} />
-      <i className="ri-search-2-line bg-green-600 rounded-full p-2 px-3 cursor-pointer hover:bg-green-700" onClick={()=>{setQuery(inputValue)}} ></i>
+      <input  type="text" className='rounded-3xl bg-green-200 py-2 px-4 border-1 border-green-600  w-60' placeholder='Search'  onChange={(e)=>setInputValue(e.target.value) } onKeyDown={handleKeyDown} />
+      <i className="ri-search-2-line bg-green-600 rounded-full p-1 px-2 text-2xl cursor-pointer hover:bg-green-700" onClick={()=>{setQuery(inputValue)}} ></i>
+      <i class="ri-home-3-fill  bg-green-600 rounded-full p-1 px-2 cursor-pointer  hover:bg-green-700 text-2xl ms-3"  onClick={()=>{navigate('/')}}></i>
+      <i class="ri-account-circle-fill  bg-green-600 rounded-full p-1 px-2 cursor-pointer  hover:bg-green-700 text-2xl"></i>
     </div>
   )
 }
