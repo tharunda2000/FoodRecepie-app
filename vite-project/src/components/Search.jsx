ad702519b1e0ce10690a264054873c51d148322d
Tharunda De Silva <tharundadesilva@gmail.com>
2025-06-28 15:19:04 +0530

    home and profile buttons edited

--- vite-project/src/components/Search.jsx
+++ vite-project/src/components/Search.jsx
@@ -38,8 +38,10 @@ const Search = ({setFoodData}) => {
     <div className='flex me-10 items-center gap-2'>
       <input  type="text" className='rounded-3xl bg-green-200 py-2 px-4 border-1 border-green-600  w-60' placeholder='Search'  onChange={(e)=>setInputValue(e.target.value) } onKeyDown={handleKeyDown} />
       <i className="ri-search-2-line bg-green-600 rounded-full p-1 px-2 text-2xl cursor-pointer hover:bg-green-700" onClick={()=>{setQuery(inputValue)}} ></i>
-      <i class="ri-home-3-fill  bg-green-600 rounded-full p-1 px-2 cursor-pointer  hover:bg-green-700 text-2xl ms-3"  onClick={()=>{navigate('/')}}></i>
-      <i class="ri-account-circle-fill  bg-green-600 rounded-full p-1 px-2 cursor-pointer  hover:bg-green-700 text-2xl"></i>
+      <div className=' bg-green-500 p-2 py-3 rounded-4xl'>
+        <i class="ri-home-3-fill  bg-green-600 rounded-full p-2 px-2 cursor-pointer  hover:bg-green-700 text-2xl m-1"  onClick={()=>{navigate('/')}}></i>
+        <i class="ri-account-circle-fill  bg-green-600 rounded-full p-2 px-2 cursor-pointer  hover:bg-green-700 text-2xl m-1"></i>
+      </div>
     </div>
   )
 }
